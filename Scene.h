a96b245b264bca79883564e9c869f13dd160d067
김주홍 <gicente1@nate.com>
2025-06-29 23:27:17 +0900

    25.06.29 home2

--- Scene.h
+++ Scene.h
@@ -3,11 +3,14 @@
 class Scene
 {
 protected:
+	//게임오브젝츠 객체로 게임오브젝트 클래스의 이것저것 접근 가능 (포인터로 선언해서)
 	std::list<GameObject*> gameObjects;
 
 public:
+	//이넘 클래스 객체 id를 선언
 	const SceneIds Id;
 
+	//초기화 할때는 씬id에 있는 이넘으로 초기화 가능
 	Scene(SceneIds id);
 	virtual ~Scene() = default;
 
--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include "stdafx.h"
-#include "SpriteGo.h"
-#include "TextGo.h"
 #include "SceneGame.h"
 
 int main()
 {
+
+    //게임 창, 이름 생성
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
     
+    // 컨테이너 클래스에 주소 넣기
     std::vector<std::string> texIds = {
         "graphics/player.png",
         "graphics/background.png"
     };
+
+    //싱클톤 리소스 매니저에 컨테이너 넣어서 이미지 및 폰트 로드시키기
     TEXTURE_MGR.Load(texIds);
     FONT_MGR.Load("fonts/KOMIKAP_.ttf");
 
+    //게임 씬 생성 및 초기화
     SceneGame sceneGame;
     sceneGame.Init();
     sceneGame.Enter();
